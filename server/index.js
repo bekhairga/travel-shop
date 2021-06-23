d56fafc2f3a28e6d22a5f2c13864082573d50ef4
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-24 04:46:31 +0500

    added basic structure for client and server

--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ app.get('/api/users/logout', auth, (req, res) => {
 		});
 	});
 });
-
-app.listen('5000', () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
 	console.log('works');
 });
