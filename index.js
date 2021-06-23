3913f7d89e06dd9bf0e762e092ec1f02b6dc7b0d
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-23 09:39:26 +0500

    user registeration route added

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+//bringing all libraries and models to use
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
 
+//connecting to database
 mongoose
 	.connect(
 		'mongodb+srv://klaun:7408449da7@cluster.g6zcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-		{ useNewUrlParser: true, useUnifiedTopology: true }
+		{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 	)
 	.then(() => {
 		console.log('MongoDB connected');
 	})
 	.catch((err) => err);
-const app = express();
 
+//using all middlewares
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
 app.get('/', (req, res) => {
 	res.send('hello world');
+});
+
+app.post('/api/users/register', (req, res) => {
+	const user = new User(req.body);
+	user.save((err, userData) => {
+		if (err) {
+			return res.json({ success: false });
+		}
+		return res.status(200).json({ success: true });
+	});
 });
 
 app.listen('5000', () => {
